5473cd7adf49a1e3647c345283d1d94c393757b0
endr1337 <144076727+endr1337@users.noreply.github.com>
2023-11-07 11:51:17 -0500

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,6 @@
  //you dont really need the popup and redirects message because they already are allowed on most computers 🤦
+//TODO LIST 
+//GET BETTER CODE
   function popUpOnWebsite()
     {
      alert('ignore the popups and redirects');
