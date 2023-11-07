085e5ab8d3d9ce9ad35655515412da4e658d65e3
endr1337 <144076727+endr1337@users.noreply.github.com>
2023-11-06 23:34:12 -0500

    Update and rename update.html to update.js

---
+++ update.js
@@ -0,0 +1,5 @@
+ //you dont really need the popup and redirects message because they already are allowed on most computers 🤦
+  function popUpOnWebsite()
+    {
+     alert('ignore the popups and redirects');
+    }
